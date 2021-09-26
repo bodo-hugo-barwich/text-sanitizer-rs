7a13a52aef5bf8648c930fba5b533fe1c91c7e88
Bodo Hugo Barwich <b.barwich@hotmail.com>
2021-09-26 11:37:47 +0100

    unicode combination tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -62,3 +62,108 @@ fn sparkle_heart_broken() {
     assert_eq!(srsout, "(?f0)(?9f)w(?96)");
 
 }
+
+
+#[test]
+fn two_hearts_center() {
+  //-------------------------------------
+  //Read the Application Parameters
+
+  let mut vrqlngs: Vec<String> = Vec::new();
+
+  let mut sopt = String::new();
+	let vsparkle_heart = vec![240, 159, 146, 150, 119, 250, 240, 159, 146, 150];
+
+
+	sopt.push_str(" -d");
+
+  vrqlngs.push(String::from("en"));
+
+
+  let srsout = sanitizer::sanitize_u8(&vsparkle_heart, &vrqlngs, &sopt);
+
+
+    println!("sparkle_heart: '{}'", srsout);
+
+    assert_eq!(srsout, "<3w(?fa)<3");
+
+}
+
+#[test]
+fn two_hearts_start() {
+  //-------------------------------------
+  //Read the Application Parameters
+
+  let mut vrqlngs: Vec<String> = Vec::new();
+
+  let mut sopt = String::new();
+	let vsparkle_heart = vec![250, 240, 159, 146, 150, 119, 240, 159, 146, 150];
+
+
+	sopt.push_str(" -d");
+
+  vrqlngs.push(String::from("en"));
+
+
+  let srsout = sanitizer::sanitize_u8(&vsparkle_heart, &vrqlngs, &sopt);
+
+
+    println!("sparkle_heart: '{}'", srsout);
+
+    assert_eq!(srsout, "(?fa)<3w<3");
+
+}
+
+#[test]
+fn two_hearts_end() {
+  //-------------------------------------
+  //Read the Application Parameters
+
+  let mut vrqlngs: Vec<String> = Vec::new();
+
+  let mut sopt = String::new();
+	let vsparkle_heart = vec![240, 159, 146, 150, 119, 240, 159, 146, 150, 250];
+
+
+	sopt.push_str(" -d");
+
+  vrqlngs.push(String::from("en"));
+
+
+  let srsout = sanitizer::sanitize_u8(&vsparkle_heart, &vrqlngs, &sopt);
+
+
+    println!("sparkle_heart: '{}'", srsout);
+
+    assert_eq!(srsout, "<3w<3(?fa)");
+
+}
+
+
+#[test]
+fn hearts_game() {
+  //-------------------------------------
+  //Read the Application Parameters
+
+  let mut vrqlngs: Vec<String> = Vec::new();
+
+  let mut sopt = String::new();
+ let vsparkle_heart = vec![
+        226, 157, 164, 240, 159, 146, 150, 119, 250, 248, 240, 159, 146, 150, 247, 190,
+    ];
+
+
+	sopt.push_str(" -d");
+
+  vrqlngs.push(String::from("en"));
+
+
+  let srsout = sanitizer::sanitize_u8(&vsparkle_heart, &vrqlngs, &sopt);
+
+
+    println!("sparkle_heart: '{}'", srsout);
+
+    assert_eq!(srsout, "<3<3w(?fa)(?f8)<3(?f7)(?be)");
+
+}
+
